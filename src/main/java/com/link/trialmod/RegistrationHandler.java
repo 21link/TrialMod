946ceaee87086ca18c6d64e07a2869e0dc3efda2
21link <66214396+21link@users.noreply.github.com>
2020-06-30 11:06:26 -0300

    Add files via upload

---
+++ src/main/java/com/link/trialmod/RegistrationHandler.java
@@ -0,0 +1,25 @@
+package com.link.trialmod;
+
+import com.link.trialmod.*;
+
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+import net.minecraftforge.event.RegistryEvent.Register;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+@EventBusSubscriber(modid = TrialMod.MODID)
+public class RegistrationHandler 
+{
+
+	@SubscribeEvent
+	public static void registerItems(Register<Item> event) 
+	{
+		final Item[] items = {
+				new Item().setRegistryName(TrialMod.MODID, "first_item").setUnlocalizedName(TrialMod.MODID + "." + "first_item").setCreativeTab(CreativeTabs.MISC)
+		};
+
+		event.getRegistry().registerAll(items);
+	}
+
+}
